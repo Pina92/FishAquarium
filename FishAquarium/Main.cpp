77f999346383c3a89976ae10cddbe3afd736eebd
Paulina Wójcik <pinawoj@gmail.com>
2017-09-26 17:57:40 +0200

    Added background and fish texture. Displaying sprites on the screen.

--- FishAquarium/Main.cpp
+++ FishAquarium/Main.cpp
@@ -5,6 +5,25 @@ using namespace sf;
 int main()
 {
 
+	//Textures
+	Texture backgroundTexture, fishTexture;
+	//obs³uga b³êdnego wczytywania
+	backgroundTexture.loadFromFile("resources/textures/background.png");
+	fishTexture.loadFromFile("resources/textures/fish1.png");
+
+	
+	
+	//Sprites
+	Sprite background, fish;
+	
+	background.setTexture(backgroundTexture);
+	
+	fish.setTexture(fishTexture);
+	fish.setScale(0.25, 0.25);
+	fish.setPosition(100, 100);
+
+	
+	
 	// Creating game window.
 	RenderWindow window(VideoMode(800, 600, 32), "FishAquarium");
 
@@ -21,8 +40,12 @@ int main()
 
 		}
 
-		window.clear(Color(30, 50, 100));
+		// Drawing everything in window.
+		//window.clear(Color(30, 50, 100));
+		window.draw(background);
+		window.draw(fish);
 		window.display();
+
 	}
 
 	return 0;
